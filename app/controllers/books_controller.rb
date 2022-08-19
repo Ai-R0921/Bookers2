ab1491480842d88f1337e48f0fa23410ca2d1a7f
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-08-19 16:44:19 +0000

    [WIP]Bootstrap導入前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,17 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path(@books)
+    redirect_to books_path
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -15,12 +28,19 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
+    @books = Book.all
   end
 
   private
 
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+
 
 end
